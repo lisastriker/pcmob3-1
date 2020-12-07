46ad061ffc26a50128d2b0d761924092acb2c83a
lisastriker <lisastriker@gmail.com>
2020-12-07 10:56:18 +0800

    added map

--- App.js
+++ App.js
@@ -2,11 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const arrayName = ["Aaran",
+"Aaren",
+"Aarez",
+"Aarman",
+"Aaron",
+"Aaron-James",
+"Aarron",
+"Aaryan",
+"Aaryn",
+"Aayan",
+"Aazaan",
+"Abaan",
+"Abbas",
+"Abdallah",
+"Abdalroof",
+"Abdihakim",
+"Abdirahman",
+"Abdisalam",
+"Abdul",
+"Abdul-Aziz",
+"Abdulbasir",
+"Abdulkadir",
+"Abdulkarem",]
 export default function App() {
   return (
     <View style={styles.container}>
+      {arrayName.map((item, key)=><Text key={key}>{item}{key}</Text>)}
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
